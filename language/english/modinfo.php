105717281862828202d5612e6151947ca3bd6391
mambax7 <mambax7@gmail.com>
2015-06-01 07:05:55 -0400

    English update

--- language/english/modinfo.php
+++ language/english/modinfo.php
@@ -10,9 +10,15 @@ define('_MI_TADFAQ_ADMENU2', 'Permission');
 define('_MI_TADFAQ_BNAME1', 'FAQ');
 define('_MI_TADFAQ_BDESC1', 'List all FAQ categories');
 
+//1.1
+define('_MI_TADFAQ_MODULE_TITLE', 'Module Title');
+define('_MI_TADFAQ_MODULE_TITLE_DESC', 'will appear in the text at the top of the module Home');
+define('_MI_TADFAQ_MODULE_TITLE_VAL', 'Frequently Asked Questions');
+
 define('_MI_TADFAQ_DIRNAME', basename(dirname(dirname(__DIR__))));
 define('_MI_TADFAQ_HELP_HEADER', __DIR__ . '/help/helpheader.html');
 define('_MI_TADFAQ_BACK_2_ADMIN', 'Back to Administration of ');
 
 //help
 define('_MI_TADFAQ_HELP_OVERVIEW', 'Overview');
+
