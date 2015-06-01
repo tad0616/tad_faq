bb6ceea500103e587d113b363d758e799c1be09c
mambax7 <mambax7@gmail.com>
2015-06-01 06:53:05 -0400

    English Updates

--- language/english/main.php
+++ language/english/main.php
@@ -1,18 +1,16 @@
 <?php
-define("_MD_TADFAQ_FAQ","FAQ");
-define("_MD_TADFAQ_FAQ_NUM","（Total: %s）");
+define('_MD_TADFAQ_FAQ', 'FAQ');
+define('_MD_TADFAQ_FAQ_NUM', '（Total: %s）');
 
-define("_MD_TADFAQ_ADD","Add FAQ");
-define("_MD_TADFAQ_CONTENT","Content");
-define("_MD_TADFAQ_ENABLE","Enable");
-define("_MD_TADFAQ_UNABLE","Unable");
-define("_MD_TADFAQ_NEW_CATE","Add Category");
+define('_MD_TADFAQ_ADD', 'Add FAQ');
+define('_MD_TADFAQ_CONTENT', 'Content');
+define('_MD_TADFAQ_ENABLE', 'Enable');
+define('_MD_TADFAQ_UNABLE', 'Unable');
+define('_MD_TADFAQ_NEW_CATE', 'Add Category');
 
-
-define("_MD_TADFAQ_CATE_MENU","Category");
-define("_MD_TADFAQ_ADD_CONTENT","Edit");
-define("_MD_TADFAQ_FAQ_TITLE","FAQ Title");
-define("_MD_TADFAQ_FAQ_ENABLE","Enabled");
-define("_MD_TADFAQ_FAQ_UNABLE","Closed");
-define("_MD_TADFAQ_NO_ACCESS_POWER","Access deny!");
-?>
+define('_MD_TADFAQ_CATE_MENU', 'Category');
+define('_MD_TADFAQ_ADD_CONTENT', 'Edit');
+define('_MD_TADFAQ_FAQ_TITLE', 'FAQ Title');
+define('_MD_TADFAQ_FAQ_ENABLE', 'Enabled');
+define('_MD_TADFAQ_FAQ_UNABLE', 'Closed');
+define('_MD_TADFAQ_NO_ACCESS_POWER', 'Access deny!');
