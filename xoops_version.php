302a5d39d457158751fc4748dae51fc4ed6f94e1
tad0616 <tad0616@gmail.com>
2021-08-22 16:33:08 +0800

    修正說明檔

--- xoops_version.php
+++ xoops_version.php
@@ -6,7 +6,7 @@ $modversion = [];
 
 //---模組基本資訊---//
 $modversion['name'] = _MI_TADFAQ_NAME;
-$modversion['version'] = 2.6;
+$modversion['version'] = 2.7;
 $modversion['description'] = _MI_TADFAQ_DESC;
 $modversion['author'] = _MI_TADFAQ_AUTHOR;
 $modversion['credits'] = _MI_TADFAQ_CREDITS;
@@ -17,7 +17,7 @@ $modversion['image'] = "images/logo_{$xoopsConfig['language']}.png";
 $modversion['dirname'] = basename(__DIR__);
 
 //---模組狀態資訊---//
-$modversion['release_date'] = '2021/08/01';
+$modversion['release_date'] = '2021-08-22';
 $modversion['module_website_url'] = 'https://tad0616.net/';
 $modversion['module_website_name'] = _MI_TAD_WEB;
 $modversion['module_status'] = 'release';
